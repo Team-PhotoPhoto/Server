d32c68f56e494b07cfa9bd7732609b4eb2fcb671
hyejinU <gpwls4306@g.skku.edu>
2022-12-22 15:32:04 +0900

    Initial commit

---
+++ src/main/java/com/hjin/photophoto/PhotophotoApplication.java
@@ -0,0 +1,13 @@
+package com.hjin.photophoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotophotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotophotoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hjin/photophoto/PhotophotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjin.photophoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotophotoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
