cfba64a167ad3585464480361b78c44f4f6aa09d
hyejinU <gpwls4306@g.skku.edu>
2023-01-21 17:37:45 +0900

    CORS security config 에 추가

--- src/main/java/com/hjin/photophoto/config/auth/SecurityConfig.java
+++ src/main/java/com/hjin/photophoto/config/auth/SecurityConfig.java
@@ -2,10 +2,16 @@ package com.hjin.photophoto.config.auth;
 
 import com.hjin.photophoto.domain.user.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.savedrequest.NullRequestCache;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -16,7 +22,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable().headers().frameOptions().disable()// h2콘솔 사용 용
+                    .csrf().disable().headers().frameOptions().disable()// h2콘솔 사용 용
+                .and()
+                    .cors()
                 .and()
                     .requestCache().requestCache(new NullRequestCache()) //쿠키...?
                 .and()
@@ -31,5 +39,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .oauth2Login()
                     .userInfoEndpoint()// 로그인 성공 이후 사용자 정보 가져올 때
                     .userService(customOAuth2UserService); //소셜 로그인 성공 후 인터페이스 구현체 등록(ex. sns에서 가져오고 싶은 사용자 정보 기능 명시 가능
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOrigin("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
