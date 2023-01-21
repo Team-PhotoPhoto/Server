e69163cc9c039ba2a36770299f6ca08564cff0cc
hyejinU <gpwls4306@g.skku.edu>
2023-01-21 17:20:29 +0900

    CORS 3000 port 추가

--- src/main/java/com/hjin/photophoto/config/WebConfig.java
+++ src/main/java/com/hjin/photophoto/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.hjin.photophoto.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,5 +17,18 @@ public class WebConfig  implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
         argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://ec2-54-180-92-252.ap-northeast-2.compute.amazonaws.com:8080",
+                        "http://localhost:3000",
+                        "https://localhost:3000",
+                        "https://127.0.0.1:3000"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+        ;
+
     }
 }
