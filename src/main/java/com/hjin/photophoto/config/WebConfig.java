c6185fa08328f379f2f2f3c0a967994dbbf348d5
hyejinU <gpwls4306@g.skku.edu>
2023-01-24 00:18:09 +0900

    config 코드 정리2

--- src/main/java/com/hjin/photophoto/config/WebConfig.java
+++ src/main/java/com/hjin/photophoto/config/WebConfig.java
@@ -11,12 +11,6 @@ import java.util.List;
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig  implements WebMvcConfigurer {
-    private final LoginUserArgumentResolver loginUserArgumentResolver;
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
-        argumentResolvers.add(loginUserArgumentResolver);
-    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
