722476e2f1d0cf4b19ad1566c3b6a4906567e13e
hyejinU <gpwls4306@g.skku.edu>
2023-02-09 20:32:36 +0900

    frontend signup.js 수정

--- src/main/frontend/src/signup/signup.js
+++ src/main/frontend/src/signup/signup.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 import styles from './signup.module.css';
-import { useRef, useEffect } from 'react';
 import BackgroundTemplate from '../common/BackgroundTemplates';
 import backgroundImage from './res/assets/background.png';
 import PPStyledButton from '../common/PPStyledButton';
@@ -13,10 +12,8 @@ import { getRandomNickname } from '../common/Utils';
 async function submitNickname(nickname) {
     const currentUserInfo = await getCurrentUserInfo();
     console.log(currentUserInfo);
-    const wallTypeCount = 4;
-    const frameTypeCount = 4;
     console.log(nickname);
-    const result = await put('/api/profile', 
+    const result = await put('/api/profile',
         {
             "nickname": nickname,
             "wallType": "BROWN",
@@ -31,41 +28,41 @@ async function submitNickname(nickname) {
 }
 
 export default function Signup() {
-    const generatedNickname = getRandomNickname();
+    const inputNicknameState = useState(getRandomNickname());
     const inputRef = useRef(null);
 
     return (
         <>
-        <BackgroundTemplate backgroundImage={backgroundImage}>
-            <div className={styles.title}>
-                <span style={{ color: "#DB60C1" }}>반</span>
-                <span style={{ color: "#FF7A00" }}>가</span>
-                <span style={{ color: "#41B8DD" }}>워</span>
-                <span style={{ color: "#8127AC" }}>요</span>
-                <span style={{ color: "#FF7A00" }}>!</span>
-            </div>
-            <PPStyledInput 
-                inputRef={inputRef}
-                className={styles.nickname_set_area}
-                title="내 닉네임" 
-                placeholder=""
-                maxLength={8}
-                defaultValue={generatedNickname}
-            />
-            <p className={styles.discription}>
-                이제 갤러리를 만들 수 있어.<br/>
-                친구들에게 나를 표현할 이름을 적어줘!
-            </p>
-            <p className={styles.notice_changable}>
-                *<span style={{ textDecoration: `underline` }}>내 프로필 수정</span>에서 언제든지 변경 가능해
-            </p>
-            <PPStyledButton 
-                type='orange' 
-                className={styles.create_gallery} 
-                text="내 갤러리 만들기" 
-                onClick={() => submitNickname(inputRef.current.value)}
-            />
-        </BackgroundTemplate>
+            <BackgroundTemplate backgroundImage={backgroundImage}>
+                <div className={styles.title}>
+                    <span style={{ color: "#DB60C1" }}>반</span>
+                    <span style={{ color: "#FF7A00" }}>가</span>
+                    <span style={{ color: "#41B8DD" }}>워</span>
+                    <span style={{ color: "#8127AC" }}>요</span>
+                    <span style={{ color: "#FF7A00" }}>!</span>
+                </div>
+                <PPStyledInput
+                    title="내 닉네임"
+                    maxLength={8}
+                    placeholder=""
+                    className={styles.nickname_set_area}
+                    inputRef={inputRef}
+                    textValueState={inputNicknameState}
+                />
+                <p className={styles.discription}>
+                    이제 갤러리를 만들 수 있어.<br/>
+                    친구들에게 나를 표현할 이름을 적어줘!
+                </p>
+                <p className={styles.notice_changable}>
+                    *<span style={{ textDecoration: `underline` }}>내 프로필 수정</span>에서 언제든지 변경 가능해
+                </p>
+                <PPStyledButton
+                    type='orange'
+                    className={styles.create_gallery}
+                    text="내 갤러리 만들기"
+                    onClick={() => submitNickname(inputRef.current.value)}
+                />
+            </BackgroundTemplate>
         </>
     )
 }
