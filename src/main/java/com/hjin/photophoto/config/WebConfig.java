e7ed3573ed023fe67f27c645663831b29a6137b6
hyejinU <gpwls4306@g.skku.edu>
2023-02-27 14:21:27 +0900

    CORS - add 80 port, photo.me, redirect to 80 port

--- src/main/java/com/hjin/photophoto/config/WebConfig.java
+++ src/main/java/com/hjin/photophoto/config/WebConfig.java
@@ -15,8 +15,9 @@ public class WebConfig  implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://ec2-54-180-92-252.ap-northeast-2.compute.amazonaws.com:8080",
+                .allowedOrigins("http://localhost:8080", "http://ec2-54-180-92-252.ap-northeast-2.compute.amazonaws.com",
                         "http://photophoto.me",
+                        "http://www.photophoto.me",
                         "http://localhost:3000"
                 )
                 .allowedOriginPatterns("*")
